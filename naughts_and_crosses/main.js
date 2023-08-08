c1819772dc441b382824775dc482ffe4eeb9b735
Alex <alexpt1234@gmail.com>
2023-08-08 22:40:48 +0100

    added ability to click on squares and an x appears

--- naughts_and_crosses/main.js
+++ naughts_and_crosses/main.js
@@ -1,2 +1,15 @@
-let randomnum = Math.random()
-document.getElementById('randomchance').innerHTML = randomnum
+let randomnum = Math.random();
+document.getElementById('randomchance').innerHTML = randomnum;
+
+const squareIdList = ['square1','square2','square3','square4','square5','square6','square7','square8','square9'];
+const squareList = [];
+
+//add all square elements to squareList
+squareIdList.forEach(square => squareList.push(document.getElementById(square)));
+
+//function to change colour of square when clicked
+const squareClick = event => event.target.innerHTML =  'X';
+
+//add eventListener to each square element
+squareList.forEach(square => square.addEventListener('click', squareClick));
+
